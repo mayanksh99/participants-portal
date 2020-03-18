768abd1d5685e1ba8f552edd930eef08fb5b5a58
Aakash Goel <goelaakash79@gmail.com>
2020-03-19 04:55:16 +0530

    --fix: different types of event can be selected

--- src/components/Events/EventsList.js
+++ src/components/Events/EventsList.js
@@ -1,25 +1,36 @@
 import React, { useEffect, useState } from "react";
 import PageTitle from "../Layout/PageTitle";
-import { Card, Row } from "antd";
+import { Card, Row, Select } from "antd";
 import "./style.css";
 import { getEventsService } from "../../utils/services";
 import { _notification } from "../../utils/_helpers";
 import Event from "./Event";
 import { Link } from "react-router-dom";
-
+const { Option } = Select;
 export default props => {
 	const [events, setEvents] = useState([]);
 	const [editDrawer, setEditDrawer] = useState(false);
 	const [eventId, setEventId] = useState(null);
 	const [refresh, toggleRefresh] = useState(false);
 	const [isLoading, setIsLoading] = useState(false);
+	const [allEvents, setAllEvents] = useState([]);
 
+	const handleChange = val => {
+		if (val === "past") {
+			setEvents(allEvents.previousEvents);
+		} else if (val === "upcoming") {
+			setEvents(allEvents.upcomingEvents);
+		} else if (val === "running") {
+			setEvents(allEvents.runningEvents);
+		}
+	};
 	useEffect(() => {
 		(async () => {
 			setIsLoading(true);
 			try {
 				const { data } = await getEventsService();
 				setEvents(data.upcomingEvents);
+				setAllEvents(data);
 				setIsLoading(false);
 			} catch (err) {
 				_notification("warning", "Error", err.message);
@@ -30,7 +41,16 @@ export default props => {
 	return (
 		<>
 			<PageTitle title="Events" />
-
+			<Select
+				style={{ minWidth: 180 }}
+				defaultValue="Upcoming Events"
+				onChange={handleChange}
+			>
+				<Option value="upcoming">Upcoming Events</Option>
+				<Option value="past">Past Events</Option>
+				<Option value="running">Running Events</Option>
+			</Select>
+			<br />
 			<div className="events-wrapper">
 				<Row gutter={[16, 16]}>
 					{events
