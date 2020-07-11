e34b945e627eb3ecc18980536d3b5f3728f3be16
Mayank Shakya <mayank.1822it1081@kiet.edu>
2020-07-12 00:20:05 +0530

    network services error handling(#6)
    add interceptor to detect error and response

--- src/components/Layout/UpdateProfile.js
+++ src/components/Layout/UpdateProfile.js
@@ -1,75 +1,15 @@
 import React, { useState, useEffect } from "react";
-import {
-	Form,
-	Button,
-	Input,
-	DatePicker,
-	Row,
-	Col,
-	Skeleton,
-	message,
-	Upload,
-	Icon,
-	Divider,
-	Select
-} from "antd";
-import styled from "styled-components";
+import { Form, Button, Input, Row, Col, Skeleton, Select } from "antd";
 import "./style.css";
-import {
-	getUserService,
-	updateUserService,
-	updateParticipantService
-} from "../../utils/services";
+import { updateParticipantService } from "../../utils/services";
 import { _notification, GET_BRANCHES, GET_YEARS } from "./../../utils/_helpers";
-import moment from "moment";
-
-const UploadContainer = styled.div`
-	align-content: center !important;
-`;
 
 const { Option } = Select;
 
 const UpdateProfile = props => {
-	const [user, setUser] = useState(null);
 	const [isLoading, setIsLoading] = useState(false);
-	const [image, setImage] = useState(null);
 	const [showSkeleton, setShowSkeleton] = useState(false);
-	const [fileList, setFileList] = useState(null);
 	const { getFieldDecorator } = props.form;
-	// const uploadprops = {
-	// 	name: "avatar",
-	// 	listType: "picture-card",
-	// 	action: "https://www.mocky.io/v2/5cc8019d300000980a055e76",
-	// 	headers: {
-	// 		authorization: "authorization-text"
-	// 	},
-	// 	beforeUpload(file) {
-	// 		const isJpgOrPng =
-	// 			file.type === "image/jpeg" || file.type === "image/png";
-	// 		if (!isJpgOrPng) {
-	// 			message.error("You can only upload JPG/PNG file!");
-	// 		}
-	// 		console.log(file);
-	// 		const isLt2M = file.size / 1024 / 1024 < 5;
-	// 		if (!isLt2M) {
-	// 			message.error("Image must smaller than 2MB!");
-	// 		}
-	// 		return isJpgOrPng && isLt2M;
-	// 	},
-	// 	onChange(info) {
-	// 		if (info.file.status === "uploading") {
-	// 		}
-	// 		if (info.file.status === "done") {
-	// 			message.success(`${info.file.name} file uploaded successfully`);
-	// 			getBase64(info.file.originFileObj, imageUrl => {
-	// 				setImage(imageUrl);
-	// 			});
-	// 		} else if (info.file.status === "error") {
-	// 			message.error(`${info.file.name} file upload failed.`);
-	// 		}
-	// 		setFileList(info.fileList);
-	// 	}
-	// };
 
 	useEffect(() => {
 		setShowSkeleton(true);
@@ -87,12 +27,6 @@ const UpdateProfile = props => {
 		}
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, []);
-
-	// const getBase64 = (img, callback) => {
-	// 	const reader = new FileReader();
-	// 	reader.addEventListener("load", () => callback(reader.result));
-	// 	reader.readAsDataURL(img);
-	// };
 
 	const handleSubmit = e => {
 		e.preventDefault();
